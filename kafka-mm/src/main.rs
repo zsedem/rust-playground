e12de00609226f8630d1ad22959c22f78fb64061
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2021-12-13 19:55:54 +0100

    fix a couple of things in kafka mirrormaker

--- kafka-mm/src/main.rs
+++ kafka-mm/src/main.rs
@@ -4,6 +4,8 @@ use futures::stream::StreamExt;
 use rdkafka::config::ClientConfig;
 use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::{CommitMode, Consumer};
+use rdkafka::error::KafkaError;
+use futures::future::Either::{Right, Left};
 use rdkafka::message::{OwnedMessage};
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use rdkafka::util::Timeout;
@@ -21,7 +23,7 @@ async fn main() {
         .version("0.1.0")
         .about("Mirrors from one kafka to another")
         .setting(AppSettings::ColorAuto)
-        .arg(Arg::with_name("source-kafka")
+        .arg(Arg::with_name("source-bootstrap-servers")
             .long("source-bootstrap-servers")
             .value_name("KAFKA_BOOTSTRAP_SERVERS")
             .help("Set to a kafka bootstrap server, where you read the topic from")
@@ -39,7 +41,7 @@ async fn main() {
             .help("Set the topic to mirror from")
             .required(true)
             .takes_value(true))
-        .arg(Arg::with_name("target-kafka")
+        .arg(Arg::with_name("target-bootstrap-servers")
             .long("target-bootstrap-servers")
             .value_name("KAFKA_BOOTSTRAP_SERVERS")
             .help("Set to a kafka bootstrap server, where you write the topic")
@@ -64,60 +66,85 @@ async fn main() {
             .takes_value(true)
             .possible_values(&vec!["earliest", "latest"])
             .default_value("earliest"))
+        .arg(Arg::with_name("compression-type")
+            .long("compression-type")
+            .value_name("COMPRESSION_TYPE")
+            .help("Compression type to choose")
+            .possible_values(&vec!["none", "gzip", "snappy", "lz4", "zstd"])
+            .takes_value(true)
+            .default_value("none"))
+        .arg(Arg::with_name("batch-size")
+            .long("batch-size")
+            .value_name("BATCH_SIZE")
+            .help("Batch size to reach for a batch to be written before linger ms time elapsed")
+            .takes_value(true)
+            .default_value("786432"))
+        .arg(Arg::with_name("batch-linger-ms")
+            .long("batch-linger-ms")
+            .value_name("BATCH_LINGER_MS")
+            .help("Milliseconds to wait for messages to be in a single batch")
+            .takes_value(true)
+            .default_value("1000"))
         .get_matches();
 
 
-    let consumer_brokers = String::from(matches.value_of("source-bootstrap-servers").expect("ArgumentError"));
-    let producer_brokers = String::from(matches.value_of("target-bootstrap-servers").expect("ArgumentError"));
-    let group_id = String::from(matches.value_of("consumer-group-id").expect("ArgumentError"));
-    let input_topic = String::from(matches.value_of("source-kafka-topic").expect("ArgumentError"));
-    let output_topic = String::from(matches.value_of("target-bootstrap-servers").expect("ArgumentError"));
-    let commit_interval_ms = matches.value_of("commit-interval-ms").expect("ArgumentError").parse()
+    let consumer_brokers = String::from(matches.value_of("source-bootstrap-servers").expect("ArgumentError \"source-bootstrap-servers\""));
+    let producer_brokers = String::from(matches.value_of("target-bootstrap-servers").expect("ArgumentError \"target-bootstrap-servers\""));
+    let group_id = String::from(matches.value_of("consumer-group-id").expect("ArgumentError \"consumer-group-id\""));
+    let input_topic = String::from(matches.value_of("source-kafka-topic").expect("ArgumentError \"source-kafka-topic\""));
+    let output_topic = String::from(matches.value_of("target-kafka-topic").expect("ArgumentError \"target-kafka-topic\""));
+    let commit_interval_ms = matches.value_of("commit-interval-ms").expect("ArgumentError \"commit-interval-ms\"").parse()
         .expect("Commit interval milliseconds must be a positive number");
-    let auto_offset_reset =  String::from(matches.value_of("auto-offset-reset").expect("ArgumentError"));
+    let auto_offset_reset =  String::from(matches.value_of("auto-offset-reset").expect("ArgumentError \"auto-offset-reset\""));
+    let compression_type = matches.value_of("compression-type").expect("ArgumentError \"compression-type\"").to_string();
+    let batch_size = matches.value_of("batch-size").expect("ArgumentError \"batch-size\"").to_string();
+    let batch_linger_ms = matches.value_of("batch-linger-ms").expect("ArgumentError \"batch-linger-ms\"").to_string();
 
     let (consumed_messages, producer_input_messages) = mpsc::channel(256);
     let (commit_messages_sender, commit_messages_receiver) = mpsc::channel(256);
 
     tokio::spawn(run_producer(
-        producer_brokers,
-        output_topic,
+        ProducerSettings {
+            topic: output_topic,
+            bootstrap_servers: producer_brokers,
+            compression_type,
+            batch_size,
+            batch_linger_ms
+        },
         producer_input_messages,
         commit_messages_sender,
     ));
 
     run_consumer(
-        consumer_brokers,
-        group_id,
-        input_topic,
-        commit_interval_ms,
-        auto_offset_reset,
+        ConsumerSettings {
+            topic: input_topic,
+            bootstrap_servers: consumer_brokers,
+            group_id,
+            commit_interval_ms,
+            auto_offset_reset
+        },
         consumed_messages,
         commit_messages_receiver,
     ).await;
 }
 
 async fn run_consumer(
-    brokers: String,
-    group_id: String,
-    input_topic: String,
-    commit_interval_ms: u64,
-    auto_offset_reset: String,
+    consumer_settings: ConsumerSettings,
     consumer_messages_sender: Sender<OwnedMessage>,
     commits_receiver: Receiver<OwnedMessage>,
 ) {
     let consumer: StreamConsumer = ClientConfig::new()
-        .set("group.id", &group_id)
-        .set("bootstrap.servers", &brokers)
+        .set("group.id", &consumer_settings.group_id)
+        .set("bootstrap.servers", &consumer_settings.bootstrap_servers)
         .set("enable.partition.eof", "false")
         .set("session.timeout.ms", "6000")
         .set("enable.auto.commit", "false")
-        .set("auto.offset.reset", auto_offset_reset)
+        .set("auto.offset.reset", &consumer_settings.auto_offset_reset)
         .create()
         .expect("Consumer creation failed");
 
     consumer
-        .subscribe(&[&input_topic])
+        .subscribe(&[&consumer_settings.topic])
         .expect("Can't subscribe to specified topic");
 
     let consuming_stream = consumer.stream().map(|x| {
@@ -125,7 +152,7 @@ async fn run_consumer(
     });
     let commits_stream = ReceiverStream::new(commits_receiver)
         .map(|x| ConsumerThreadTrigger::MessageDoneProcessing(x));
-    let commits_trigger_stream = create_periodic_commit_trigger(commit_interval_ms);
+    let commits_trigger_stream = create_periodic_commit_trigger(consumer_settings.commit_interval_ms);
 
     let mut consumer_stream = {
         select(
@@ -162,26 +189,45 @@ async fn run_consumer(
 }
 
 async fn run_producer(
-    brokers: String,
-    output_topic: String,
-    mut consumed_messages_receiver: Receiver<OwnedMessage>,
+    producer_settings: ProducerSettings,
+    consumed_messages_receiver: Receiver<OwnedMessage>,
     commit_messages_sender: Sender<OwnedMessage>,
 ) {
     let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", &brokers)
+        .set("bootstrap.servers", &producer_settings.bootstrap_servers)
         .set("message.timeout.ms", "5000")
-        .set("linger.ms", "2000")
-        .set("batch.size", "131072")
+        .set("linger.ms", &producer_settings.batch_linger_ms)
+        .set("batch.size", &producer_settings.batch_size)
+        .set("compression.type", &producer_settings.compression_type)
         .create()
         .expect("Producer creation error");
 
-    while let Some(owned_message) = consumed_messages_receiver.recv().await {
-        tokio::spawn(handle_message(
-            output_topic.clone(),
-            producer.clone(),
-            owned_message,
-            commit_messages_sender.clone(),
-        ));
+    let (producer_died_sender, producer_died_receiver) = mpsc::channel(1);
+    let mut producer_events_receiver = select(
+        ReceiverStream::new(producer_died_receiver).map(|x| Left(x)),
+        ReceiverStream::new(consumed_messages_receiver).map(|x| Right(x))
+    );
+
+    while let Some(message) = producer_events_receiver.next().await {
+        match message {
+            Right(owned_message) => {
+                tokio::spawn(handle_message(
+                    producer_settings.topic.clone(),
+                    producer.clone(),
+                    owned_message,
+                    commit_messages_sender.clone(),
+                    producer_died_sender.clone()
+                ));
+            }
+            Left((err, owned_message)) => {
+                panic!(
+                    "Error while producing message (partition: {}, offset: {}): {}",
+                    owned_message.partition(),
+                    owned_message.offset(),
+                    err
+                );
+            }
+        }
     }
 }
 
@@ -190,6 +236,7 @@ async fn handle_message(
     producer: FutureProducer,
     owned_message: OwnedMessage,
     commit_messages_sender: Sender<OwnedMessage>,
+    producer_died_sender: Sender<(KafkaError, OwnedMessage)>,
 ) {
     let mut record: FutureRecord<[u8], [u8]> = FutureRecord::to(&output_topic);
     if owned_message.key().is_some() {
@@ -198,14 +245,20 @@ async fn handle_message(
     if owned_message.payload().is_some() {
         record = record.payload(owned_message.payload().expect("internalerror"));
     }
-    producer
+    let r = producer
         .send(record, Timeout::Never)
-        .await
-        .expect("producer error");
-    commit_messages_sender
-        .send(owned_message)
-        .await
-        .expect("internal channel error on commits");
+        .await;
+    match r {
+        Err(e) => {
+            producer_died_sender.send(e.clone()).await.expect("producer message failure failed to send");
+        }
+        Ok(_) => {
+            commit_messages_sender
+                .send(owned_message)
+                .await
+                .expect("internal channel error on commits");
+        }
+    }
 }
 
 fn create_periodic_commit_trigger(commit_interval_ms: u64) -> Map<IntervalStream, fn(tokio::time::Instant) -> ConsumerThreadTrigger> {
@@ -217,4 +270,20 @@ enum ConsumerThreadTrigger {
     MessageToConsume(OwnedMessage),
     MessageDoneProcessing(OwnedMessage),
     TimerEvent,
+}
+
+struct ProducerSettings {
+    topic: String,
+    bootstrap_servers: String,
+    compression_type: String,
+    batch_size: String,
+    batch_linger_ms: String
+}
+
+struct ConsumerSettings {
+    topic: String,
+    bootstrap_servers: String,
+    group_id: String,
+    commit_interval_ms: u64,
+    auto_offset_reset: String
 }
